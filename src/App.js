a2629d02e8b4eb4579da062727c2bf1b7d550a54
PastaiaUci <lucadavidstefan21@yahoo.com>
2024-09-03 03:16:32 +0300

    huh?

--- src/App.js
+++ src/App.js
@@ -1,35 +1,30 @@
-import React, { useEffect } from 'react';
-import AOS from 'aos';
+import React, { useEffect } from "react";
+import AOS from "aos";
 import "aos/dist/aos.css";
-import './index.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from 'react-router-dom';
+import "./index.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import DemoProduct from './pages/DemoProduct';
-import AboutUs from './pages/AboutUs';
-import Services from './pages/Services';
-import OurTeam from './pages/OurTeam';
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import DemoProduct from "./pages/DemoProduct";
+import AboutUs from "./pages/AboutUs";
+import Services from "./pages/Services";
+import OurTeam from "./pages/OurTeam";
 
-import {useDocTitle} from './components/CustomHook';
-import ScrollToTop from './components/ScrollToTop';
+import { useDocTitle } from "./components/CustomHook";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
   useEffect(() => {
-    
     const aos_init = () => {
       AOS.init({
         once: true,
         duration: 1000,
-        easing: 'ease-out-cubic',
+        easing: "ease-out-cubic",
       });
-    }
+    };
 
-    window.addEventListener('load', () => {
+    window.addEventListener("load", () => {
       aos_init();
     });
   }, []);
@@ -41,7 +36,6 @@ function App() {
       <Router>
         <ScrollToTop>
           <Routes>
-            
             {/* For deplyoyment on GH Pages use this + add on package.json this: "homepage": "https://pastaiauci.github.io/baghii-website", */}
             <Route path="/baghii-website" element={<Home />} />
             {/* <Route path="/" element={<Home />} /> */}
@@ -56,6 +50,5 @@ function App() {
     </>
   );
 }
-
 
 export default App;
